adc8102ced54fe2f583dcfb54aedc7bc9d9bf5ba
Emanuele Vernetti <e.vernetti.1033@vallauri.edu>
2020-03-12 14:51:02 +0100

    OpenXml

---
+++ OpenXmlPlayground/FormMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OpenXmlPlayground
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSimpleWordTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSimpleWordTest
+            // 
+            this.btnSimpleWordTest.Location = new System.Drawing.Point(12, 12);
+            this.btnSimpleWordTest.Name = "btnSimpleWordTest";
+            this.btnSimpleWordTest.Size = new System.Drawing.Size(460, 23);
+            this.btnSimpleWordTest.TabIndex = 0;
+            this.btnSimpleWordTest.Text = "SIMPLE WORD DOCUMENT TEST";
+            this.btnSimpleWordTest.UseVisualStyleBackColor = true;
+            this.btnSimpleWordTest.Click += new System.EventHandler(this.btnSimpleWordTest_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.btnSimpleWordTest);
+            this.Name = "FormMain";
+            this.Text = "OpenXML Playground";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimpleWordTest;
+    }
+}
+
---
+++ OpenXmlUtilities/ClsOpenXmlUtilities.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Color = DocumentFormat.OpenXml.Wordprocessing.Color;
+
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+
+namespace OpenXmlUtilities
+{
+    public class ClsOpenXmlUtilities
+    {
+        public static void AddStyle(MainDocumentPart mainPart, string styleId, string styleName, string fontName, int fontSize, string rgbColor, bool isBold, bool isItalic, bool isUnderlined)
+        {
+            // we have to set the properties
+            RunProperties rPr = new RunProperties();
+            Color color = new Color() { Val = rgbColor }; // the color is red
+            RunFonts rFont = new RunFonts();
+            rFont.Ascii = fontName; // the font is Arial
+            rPr.Append(color);
+            rPr.Append(rFont);
+            rPr.Append(new FontSize() { Val = (fontSize*2).ToString() }); //font size (in 1/72 of an inch)
+            if (isBold)
+            {
+                rPr.Append(new Bold()); // it is Bold
+            }
+            if (isItalic)
+            {
+                rPr.Append(new Italic());
+            }
+            if (isUnderlined)
+            {
+                rPr.Append(new Underline() { Val = UnderlineValues.Single });
+            }
+
+            Style style = new Style();
+            style.StyleId = styleId; //this is the ID of the style
+            style.Append(new Name() { Val = styleName }); //this is the name of the new style
+            style.Append(rPr); //we are adding properties previously defined
+
+            // we have to add style that we have created to the StylePart
+            StyleDefinitionsPart stylePart;
+            if (mainPart.StyleDefinitionsPart == null) 
+            {
+                stylePart = mainPart.AddNewPart<StyleDefinitionsPart>();
+                stylePart.Styles = new Styles();
+            }
+            else
+            {
+                stylePart = mainPart.StyleDefinitionsPart;
+            }
+            stylePart.Styles.Append(style);
+            stylePart.Styles.Save(); // we save the style part
+        }
+
+        public static Paragraph CreateParagraphWithStyle(string styleId, JustificationValues justification)
+        {
+            Paragraph paragraph = new Paragraph();
+            ParagraphProperties pp = new ParagraphProperties();
+            paragraph.Append(pp);
+            Run r = new Run();
+            // we set the style
+            pp.ParagraphStyleId = new ParagraphStyleId() { Val = styleId };
+            // we set the alignement
+            pp.Justification = new Justification() { Val = justification };
+            return paragraph;
+        }
+
+        public static void AddTextToParagraph(Paragraph paragraph, string content)
+        {
+            Run r = new Run();
+            Text t = new Text(content);
+            r.Append(t);
+            paragraph.Append(r);
+        }
+
+        public static void InsertPicture(WordprocessingDocument wordprocessingDocument, string fileName)
+        {
+            MainDocumentPart mainPart = wordprocessingDocument.MainDocumentPart;
+            ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+            using (FileStream stream = new FileStream(fileName, FileMode.Open))
+            {
+                imagePart.FeedData(stream);
+            }
+            AddImageToBody(wordprocessingDocument, mainPart.GetIdOfPart(imagePart));
+        }
+
+        private static void AddImageToBody(WordprocessingDocument wordDoc, string relationshipId)
+        {
+            // Define the reference of the image.
+            var element =
+                 new Drawing(
+                     new DW.Inline(
+                         new DW.Extent() { Cx = 990000L, Cy = 792000L },
+                         new DW.EffectExtent()
+                         {
+                             LeftEdge = 0L,
+                             TopEdge = 0L,
+                             RightEdge = 0L,
+                             BottomEdge = 0L
+                         },
+                         new DW.DocProperties()
+                         {
+                             Id = (UInt32Value)1U,
+                             Name = "Picture 1"
+                         },
+                         new DW.NonVisualGraphicFrameDrawingProperties(
+                             new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                         new A.Graphic(
+                             new A.GraphicData(
+                                 new PIC.Picture(
+                                     new PIC.NonVisualPictureProperties(
+                                         new PIC.NonVisualDrawingProperties()
+                                         {
+                                             Id = (UInt32Value)0U,
+                                             Name = "New Bitmap Image.jpg"
+                                         },
+                                         new PIC.NonVisualPictureDrawingProperties()),
+                                     new PIC.BlipFill(
+                                         new A.Blip(
+                                             new A.BlipExtensionList(
+                                                 new A.BlipExtension()
+                                                 {
+                                                     Uri =
+                                                        "{28A0092B-C50C-407E-A947-70E740481C1C}"
+                                                 })
+                                         )
+                                         {
+                                             Embed = relationshipId,
+                                             CompressionState =
+                                             A.BlipCompressionValues.Print
+                                         },
+                                         new A.Stretch(
+                                             new A.FillRectangle())),
+                                     new PIC.ShapeProperties(
+                                         new A.Transform2D(
+                                             new A.Offset() { X = 0L, Y = 0L },
+                                             new A.Extents() { Cx = 990000L, Cy = 792000L }),
+                                         new A.PresetGeometry(
+                                             new A.AdjustValueList()
+                                         )
+                                         { Preset = A.ShapeTypeValues.Rectangle }))
+                             )
+                             { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+                     )
+                     {
+                         DistanceFromTop = (UInt32Value)0U,
+                         DistanceFromBottom = (UInt32Value)0U,
+                         DistanceFromLeft = (UInt32Value)0U,
+                         DistanceFromRight = (UInt32Value)0U,
+                         EditId = "50D07946"
+                     });
+
+            // Append the reference to body, the element should be in a Run.
+            wordDoc.MainDocumentPart.Document.Body.AppendChild(new Paragraph(new Run(element)));
+        }
+    }
+}
